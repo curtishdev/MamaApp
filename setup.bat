c735534bd1e07ca3b6f324bed8c4b057253fd1a3
CurtishDEV <54275918+curtishdev@users.noreply.github.com>
2024-01-26 11:49:44 +0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,11 +1,19 @@
-﻿@echo off
+@echo off
 cls
+
+REM Check if AutoHotKey is installed
+where AutoHotKey >nul 2>nul
+if errorlevel 1 (
+    echo Установите AutoHotkey перед использованием установщика!
+    pause
+    exit
+)
 
 NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' (
     goto continue
 ) else (
-    echo Установка AutoHotkey...
+    echo Установка и настройка приложения...
     timeout /t 1 >nul
     start "" "powershell" -Command "Start-Process '%0' -Verb RunAs"
     exit
@@ -16,21 +24,6 @@ setlocal enabledelayedexpansion
 
 set mobilnik_dest=C:\Users\mobilnik.mp3
 set mama_dest=C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\mama.ahk
-set autohotkey_exe_dest=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AutoHotkey\AutoHotkeyU64.exe
-
-powershell -Command "& { Invoke-WebRequest -Uri 'https://www.autohotkey.com/download/ahk-install.exe' -OutFile 'ahk-install.exe' }"
-
-if %errorlevel% neq 0 (
-    echo Ошибка! Не удалось загрузить установщик AutoHotkey.
-    pause
-    exit
-)
-
-echo Установка AutoHotkey...
-start /wait "" "ahk-install.exe" /S
-
-REM
-del "ahk-install.exe"
 
 powershell -Command "& { Invoke-WebRequest -Uri 'https://github.com/curtishdev/MamaApp/raw/main/mobilnik.mp3' -OutFile '!mobilnik_dest!' }"
 
@@ -48,20 +41,7 @@ if %errorlevel% neq 0 (
     exit
 )
 
-powershell -Command "& { Invoke-WebRequest -Uri 'https://github.com/curtishdev/MamaApp/raw/main/AutoHotkeyU64.exe' -OutFile '!autohotkey_exe_dest!' }"
-
-if %errorlevel% neq 0 (
-    echo Ошибка! AutoHotkeyU64.exe не найден.
-    pause
-    exit
-)
-
-REM
-taskkill /F /IM AutoHotkeyU64.exe 2>nul
-
-echo Установите AutoHotkey! 
-echo Нажмите [Enter] чтобы продолжить.
-pause
+start "" "!mama_dest!"
 
 exit
 
